921336a573760ebe0f91a33d1b295138c39100cb
yash <signinwithyash1178@gmail.com>
2025-02-03 12:45:15 +0530

    util files added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed",err);
+})
+/*
 import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
+import { DB_NAME } from './constants.js';
 
 import express from 'express';
 
@@ -21,3 +37,4 @@ const app = express()
             throw error
     }
 })
+    */
